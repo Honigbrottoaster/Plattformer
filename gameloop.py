c0461d9e4206ba0791660026dba8a86d08e1dec8
Honigbrottoaster <jonas@honigbrottoaster.de>
2024-01-22 16:04:56 +0100

    adding jumping

--- gameloop.py
+++ gameloop.py
@@ -3,8 +3,37 @@ import sys
 from settings import *
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, floor):
+        self.width = 30
+        self.height = 50
+        self.x_pos = 100
+        self.y_pos = SCREEN_HEIGHT - floor.height - self.height
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
+        self.on_ground = True
+        self.gravity = 0.4
+        self.jump_force = 10
+        self.y_input = 0
+    
+    def get_onGround(self, floor):
+        if self.rect.colliderect(floor.rect):
+            self.on_ground = True
+            self.rect.y = floor.rect.y - self.height
+        else:
+            self.on_ground = False
+    
+    def move(self):
+        self.rect.y += self.y_input
+
+        self.get_onGround(game.floor)
+
+        if not self.on_ground:
+            self.y_input += self.gravity
+        else:
+            self.y_input = 0
+
+    def jump(self):
+        if self.on_ground:
+            self.y_input = -self.jump_force
 
 class Floor:
     def __init__(self):
@@ -42,27 +71,39 @@ class Game:
         #ground
         self.floor = Floor()
 
+        #player
+        self.player = Player(self.floor)
+
     def run(self):
         self.handleEvents()
-        self.draw()
         self.update()
+        self.draw() 
 
     def handleEvents(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.jump()
 
     def draw(self):
         self.screen.fill((70, 100, 10))
 
         self.platform.draw(self)
 
+        pygame.draw.rect(self.screen, "black", self.player.rect)
+
         pygame.draw.rect(self.screen, "red", self.floor.rect)
 
     def update(self):
         #platform 
         self.platform.move()
+
+        #player
+        self.player.get_onGround(self.floor)
+        self.player.move()
 
         pygame.display.update()
         self.clock.tick(60)
